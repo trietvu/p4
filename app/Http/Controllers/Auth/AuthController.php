425c641525ccbcfa8e1f1224d4c0d73c8bd0eb47
Triet Vu <tvu@fas.harvard.edu>
2015-12-19 23:10:52 -0600

    undo change to Authcontroller

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -23,8 +23,6 @@ class AuthController extends Controller
 
     use AuthenticatesAndRegistersUsers, ThrottlesLogins;
 
-    #Where the user will be redirected to after registration.
-    protected $redirectTo = '/addresses/create';
     # Where should the user be redirected to if their login succeeds?
     protected $redirectPath = '/accounts';
 
