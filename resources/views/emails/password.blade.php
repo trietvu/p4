07ab9b9a726034bbe8d1a3243c86b71075e0eda6
Triet Vu <tvu@fas.harvard.edu>
2015-12-20 01:06:04 -0600

    i am tired

--- resources/views/emails/password.blade.php
+++ resources/views/emails/password.blade.php
@@ -1,53 +1,6 @@
-@extends('layouts.master')
+Dear User,
 
-@section('content')
-<div class="stage">
-    <div class="pageContent">
-        <div class="pageTitle"><h2>My First Ledger</h2></div>
-            <h1>Reset your password</h1>
+Oh no! you've forgotten your password. No big deal.
+Click on the following URL to reset it.
 
-            @if(count($errors) > 0)
-                <ul class='errors'>
-                    @foreach ($errors->all() as $error)
-                        <li><span class='fa fa-exclamation-circle'></span> {{ $error }}</li>
-                    @endforeach
-                </ul>
-            @endif
-
-                <form method="POST" action="/password/reset">
-                    {!! csrf_field() !!}
-                    <input type="hidden" name="token" value="{{ $token }}">
-
-                    @if (count($errors) > 0)
-                        <ul>
-                            @foreach ($errors->all() as $error)
-                                <li>{{ $error }}</li>
-                            @endforeach
-                        </ul>
-                    @endif
-
-                    <div class='form-group'>
-                        Email
-                        <input type="email" name="email" value="{{ old('email') }}">
-                    </div>
-
-                    <div class='form-group'>
-                        Password
-                        <input type="password" name="password">
-                    </div>
-
-                    <div class='form-group'>
-                        Confirm Password
-                        <input type="password" name="password_confirmation">
-                    </div>
-
-                    <div class='form-group'>
-                        <button type="submit">
-                            Reset Password
-                        </button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
-@stop
+{{ url('password/reset/'.$token) }}
